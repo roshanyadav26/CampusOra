b667f2c0c70f4cc73bfa300491f3a374142f2dc2
roshanyadav26 <ajayroshan25801@gmail.com>
2026-02-22 11:06:21 +0530

    UPDATED THE LOCAL HOST LINK

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 /* ================= SOCKET.IO ================= */
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+origin: process.env.CLIENT_URL,
     methods: ["GET", "POST"],
   },
 });
@@ -80,7 +80,7 @@ io.on("connection", (socket) => {
 /* ================= MIDDLEWARE ================= */
 app.use(
   cors({
-    origin: "http://localhost:3000",
+origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
