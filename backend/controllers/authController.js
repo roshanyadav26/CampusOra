556e2b3d6fb401b695b8b4d2945542e56ca28e02
roshanyadav26 <ajayroshan25801@gmail.com>
2026-03-05 11:34:23 +0530

    fix smtp port

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,11 +8,18 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
+  port: Number(process.env.SMTP_PORT),
   secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
+  }
+});
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log("SMTP ERROR:", error);
+  } else {
+    console.log("SMTP SERVER READY");
   }
 });
 
