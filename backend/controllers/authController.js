dc618e2b9cc3d4f586ab3a8467959cbd761ce95e
roshanyadav26 <ajayroshan25801@gmail.com>
2026-03-05 09:47:23 +0530

    fix gmail transporter for render

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,9 +7,7 @@ const nodemailer = require("nodemailer");
 /* ================= EMAIL TRANSPORT ================= */
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
